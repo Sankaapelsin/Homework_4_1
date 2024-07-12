cf062437e2afb886bd007aa2b062869c52a51efb
Aleksandr <sanka.apelsin@mail.ru>
2024-07-13 02:05:15 +0900

    'Second commit'

---
+++ classes/class_product.py
@@ -0,0 +1,16 @@
+class Product:
+    name: str
+    description: str
+    price: int
+    quantity: int
+
+    def __init__(self, name, description, price, quantity):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.quantity = quantity
+
+    def __str__(self):
+        return f"{self.name} {self.description} {self.price} {self.quantity}"
+
+    __repr__ = __str__
--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,22 @@
+from classes.class_category import Category
+from classes.class_product import Product
 import json
 
 
-def load():
-    with open(r'C:\Users\User\Desktop\PyCharm_Projects\Term_4\13_1\Examples\data\data.json',
-              'r', encoding='utf8') as file:
-        print(file)
-        operations = json.load(file)
-        return operations
-
-
-print(load)
+def func():
+    with open('/data/products.json', 'r', encoding='utf-8') as file:
+        data = json.load(file)
+        print(data)
+        for category_data in data:
+            category_name = category_data['name']
+            category_description = category_data['description']
+            category_products = []
+            for product_data in category_data['products']:
+                product = Product(product_data['name'], product_data['description'], product_data['price'],
+                                  product_data['quantity'])
+                category_products.append(product)
+                print(product)
+            category = Category(category_name, category_description, category_products)
+            print(category)
+        print(Category.total_categories)
+        print(Category.total_unique_products)
