40086749f33875503631ab9a052d47527dbba459
Aleksand Batuev <sankaapelsin@gmail.com>
2024-07-14 03:07:32 +0300

    'utils changes'

--- src/utils.py
+++ src/utils.py
@@ -3,20 +3,26 @@ from classes.class_product import Product
 import json
 
 
-def func():
-    with open('/data/products.json', 'r', encoding='utf-8') as file:
+def load():
+    with open('data/products.json', 'r', encoding='utf-8') as file:
         data = json.load(file)
-        print(data)
-        for category_data in data:
-            category_name = category_data['name']
-            category_description = category_data['description']
-            category_products = []
-            for product_data in category_data['products']:
-                product = Product(product_data['name'], product_data['description'], product_data['price'],
-                                  product_data['quantity'])
-                category_products.append(product)
-                print(product)
-            category = Category(category_name, category_description, category_products)
-            print(category)
-        print(Category.total_categories)
-        print(Category.total_unique_products)
+        return data
+
+
+def func():
+    data = load()
+    for category_data in data:
+        category_name = category_data['name']
+        category_description = category_data['description']
+        category_products = []
+        for product_data in category_data['products']:
+            product = Product(product_data['name'], product_data['description'], product_data['price'],
+                              product_data['quantity'])
+            category_products.append(product.name)
+            category_products.append(product.description)
+            category_products.append(product.price)
+            category_products.append(product.quantity)
+        category = Category(category_name, category_description, category_products)
+        print(category)
+    print(f"Количество категорий：{Category.total_categories}")
+    print(f"Количество уникальных товаров：{Category.total_unique_products}")
